a92766ce5cd945f5bbef4e8b85e37138a531bb42
ayush <yaushsingh2077@gmail.com>
2021-10-24 22:59:50 +0545

    commit with integer, bool

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -26,4 +26,51 @@ fn main() {
     // rust uses const keyword for concept of constants
     const MY_YEAR : u32 = 34;
 
+    abt_datatypes();
+
+  let sum = addition(4,27);
+    println!("the sum is {}", sum);
 }
+
+//preparing function 
+fn abt_datatypes(){
+    /* rust has scalar and compund datatypes
+    scalar types : integer, floating-point, boolean, characters
+    
+    */
+    let a = 2;
+    let b: u32 = 4;
+    let sum = a+b;
+    println!("the sum of {} and {} is {}",a,b,sum);
+    let c = 5.66;
+    let d = 4.5;
+    let diff = c-d;
+    println!("the difference of {} and {} is {}", c,d,diff);
+
+    let t = true;
+    let x = false;
+    println!("this is {} as well as {}",t,x );
+
+    //compound datatypes
+    let tup = ("ayush", 34);
+    //tuple can be accessed by destructuring and dot notation 
+
+    // destructuring 
+    let (name, age) = tup;
+    //dot notation
+    let age = tup.1;
+    println!("my name is {} and i am {} years old.", tup.0, tup.1);
+
+
+    let error_codes= [401,402,404,405];
+     let not_found = error_codes[2];
+     println!("error code {}",not_found);
+} 
+
+//simple add function that returns sum
+fn addition(x: i32 ,y:i32)-> i32{
+    /* return x+y can be written as below because in rust by default
+     last line of function is retrun value*/
+    x +y
+}
+    
